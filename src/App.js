1f214849f8c7a84f0fb45ceb07d479a7e02592fb
benharlucas <benhar.masahod@msugensan.edu.ph>
2022-04-18 11:28:25 +0800

    App and index toggle update

--- src/App.js
+++ src/App.js
@@ -8,27 +8,23 @@ function App() {
 
   const [diary, setDiary] = useState('')
   
-  const [example, setExample]=
-  useState('')
-  useEffect(() => {
-    console.log('Render')
+  const [showForm, setShowForm]=
+  useState(true)
 
-  return() => {
-    console.log('Unmount')
-  }
-  },[example])
-  const changeHandler = (e) => {
+
+  const changeHandler =(e) => {
     setDiary(e.target.value)
   }
-
   return (
     <>
     <Header />
     <main>
-    <input name='example' value=
-    {example} onChange={(e) =>
-    setExample(e.target.value)}/>
-    <Form myDiary={diary } handleChange={changeHandler}/>
+    <button onClick={() => setShowForm
+    (!showForm)} >Toggle</button>
+    {showForm &&
+    <Form myDiary={diary}
+     handleChange={changeHandler}/>
+    }
     </main>
     <Footer />
     </>
